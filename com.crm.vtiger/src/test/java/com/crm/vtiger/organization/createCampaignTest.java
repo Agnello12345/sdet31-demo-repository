43e58a8e9fbf73677db804eb9cdad264ef678fde
PC <PC@DESKTOP-HH2KTU2>
2022-03-24 09:27:00 +0530

    create campaign pushed

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/organization/createCampaignTest.java
@@ -0,0 +1,13 @@
+package com.crm.vtiger.organization;
+
+import org.testng.annotations.Test;
+
+public class createCampaignTest 
+{
+	@Test
+	public void createCamp()
+	{
+		System.out.println("campaign created");
+	}
+
+}
