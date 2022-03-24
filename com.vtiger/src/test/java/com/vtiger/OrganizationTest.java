cf929ce210729799ade5176d970bce37a94abbf7
PC <PC@DESKTOP-HH2KTU2>
2022-03-24 13:37:06 +0530

    commit 1

---
+++ com.vtiger/src/test/java/com/vtiger/OrganizationTest.java
@@ -0,0 +1,13 @@
+package com.vtiger;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest 
+{
+	@Test
+	public void createorg()
+	{
+		System.out.println("add organization");
+	}
+
+}
